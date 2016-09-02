09cd1121d0c1c4bd38fc38946b038db2ee18b88c
SeeSharpist <KNeithamer@mi.corp.rockfin.com>
2016-09-02 14:16:20 -0400

    init

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kyletest1.Startup))]
+namespace kyletest1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
